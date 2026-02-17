094fd95eeec6531865af0b096b8584671205221a
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2026-02-17 09:33:55 +0200

    prepare module 12 lesson 02

---
+++ javascript/module-12/lesson-01/src/js/04-quiz.js
@@ -0,0 +1,25 @@
+// import axios from "axios";
+
+const getUsers = async () => {
+  const url = "https://dummyjson.com/users";
+  // const resposne = await axios.get(url, { responseType: "text" });
+  // console.log("🚀 ~ getUsers ~ resposne:", resposne.data);
+
+  // const response = await fetch(url);
+  // console.log("🚀 ~ getUsers ~ response:", response);
+  // const json = await response.json();
+  // const text = await response.text();
+  // console.log("🚀 ~ getUsers ~ text:", text);
+  // console.log("🚀 ~ getUsers ~ json:", json);
+};
+
+getUsers();
+
+//! ТИМЧАСОВО
+const FURNITURE_URL =
+  "https://furniture-store-v2.b.goit.study/api/furnitures?page=1&limit=10";
+
+//! Response schema
+// {
+//   (title, imageUrl);
+// }
